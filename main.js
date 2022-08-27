f676482948a9061f0cccf76a678fa03074e861bd
Farai Takawira <faraitaks@gmail.com>
2022-08-27 10:26:12 +0200

    Edited js file
    Questions for Practice Purposes

--- main.js
+++ main.js
@@ -1,8 +1,57 @@
- let toggle = document.querySelector('.toggle');
- let menu = document.querySelector('.menu');
+// Write a JavaScript program to change the capitalization of all letters in a given string. 
 
-toggle.onclick = function(){
-    menu.classList.toggle('active');
-}
 
-// LETS CODE TOGETHER
+//Write a JavaScript program to change the characters (lower case) 
+//in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a
+
+
+
+// function change_char(str1) {
+// 	const result = [];
+// 	for (let i = 0; i < str1.length; i++)
+//     {
+//         const char_order = str1.charCodeAt(i) - 'a'.charCodeAt(0);
+//         const change_char = 25 - char_order + 'a'.charCodeAt(0);
+//         result.push(String.fromCharCode(change_char));
+//     }
+// 	return result.join("");
+// }
+// console.log(change_char("abcxyz"));
+// console.log(change_char("python")); 
+
+
+
+
+//Write a JavaScript program to swap two halves of a given array of integers of even length.
+
+
+
+
+// Write a JavaScript program to display the current day and time in the following format.  Go to the editor
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+
+
+
+
+
+
+
+// Write a JavaScript program to swap pairs of adjacent digits of a given integer of even length
+
+
+
+
+
+
+
+
+
+
+// Write a JavaScript program to compute the sum of all digits that occur in a given string
+
+
+
+// Write a JavaScript program to break an address of an url and put it's part into an array
+// Note: url structure : ://.org[/] and there may be no part in the address.
